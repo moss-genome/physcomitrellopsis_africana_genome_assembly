1597e496eed0e3cb1683bf1bccd27a128d6630ca
AStarovoitov <50898446+AStarovoitov@users.noreply.github.com>
2019-08-08 14:56:32 -0400

    Add files via upload

---
+++ flye_assembly_moss_30JAN19_5074.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=flye_moss_30JAN19_Pafricana
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 36
+#SBATCH --partition=himem
+#SBATCH --qos=himem
+#SBATCH --mail-type=END
+#SBATCH --mem=450G
+#SBATCH --mail-user=andrew.starovoitov@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+module load flye/2.4.2
+flye --nano-raw /labs/Wegrzyn/Moss/Physcomitrellopsis_africana/Physcomitrellopsis_africana_Genome/RawData_Nanopore_5074/5074_test_LSK109_30JAN19/5074_test_LSK109_30JAN19-reads/5074_test_LSK109_30JAN19-reads-pass.fastq  --genome-size 1g --out-dir /labs/Wegrzyn/Moss/Physcomitrellopsis_africana/Physcomitrellopsis_africana_Genome/RawData_Nanopore_5074/5074_test_LSK109_30JAN19/flye_assembly/ --threads 36
---
+++ nanopolish_index_physcomitrellopsis_africana.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=nanopolish_index_physcomitrellopsis_africana
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 1
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --mail-type=END
+#SBATCH --mem=250G
+#SBATCH --mail-user=andrew.starovoitov@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+module load nanopolish/0.11.1
+
+nanopolish index -d /labs/Wegrzyn/Moss/Physcomitrellopsis_africana/Physcomitrellopsis_africana_Genome/RawData_Nanopore_5074/5074_test_LSK109_30JAN19/5074_test_LSK109_30JAN19-fast5_pass/5074_test_LSK109_30JAN19/20190130_1817_1-A1-D1_PAD10479_cfde2fcf/fast5_pass/ /labs/Wegrzyn/Moss/Physcomitrellopsis_africana/Physcomitrellopsis_africana_Genome/RawData_Nanopore_5074/5074_test_LSK109_30JAN19/5074_test_LSK109_30JAN19-reads/5074_test_LSK109_30JAN19-reads-pass.fastq 
