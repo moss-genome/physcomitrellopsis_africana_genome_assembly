d49e6c63f2f9eaca188515dd4a2d541f8c802da6
AStarovoitov <50898446+AStarovoitov@users.noreply.github.com>
2020-01-06 16:21:36 -0500

    Update centrifuge_pafricana.sh

--- centrifuge_pafricana.sh
+++ centrifuge_pafricana.sh
@@ -13,4 +13,4 @@
 
 module load centrifuge/1.0.4-beta
 
-centrifuge -f -x p+h+v --report-file centrifuge_pafricana_polished_3kb_report.tsv --quiet --min-hitlen 50 /labs/Wegrzyn/Moss/Physcomitrellopsis_africana/Physcomitrellopsis_africana_Genome/RawData_Nanopore_5074/5074_test_LSK109_30JAN19/polished_assembly/physcomitrellopsis_africana_polished_3kb_assembly.fasta
+centrifuge -f -x p+h+v --report-file centrifuge_pafricana_polished_3kb_report.tsv --quiet --min-hitlen 50 /projects/EBP/Wegrzyn/Moss/Physcomitrellopsis_africana/Physcomitrellopsis_africana_Genome/RawData_Nanopore_5074/5074_test_LSK109_30JAN19/polished_assembly/physcomitrellopsis_africana_polished_3kb_assembly.fasta
