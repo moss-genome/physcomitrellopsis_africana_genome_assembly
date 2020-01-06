98023ac21341652d24db9062ea0a7653be129daa
AStarovoitov <50898446+AStarovoitov@users.noreply.github.com>
2020-01-06 16:31:29 -0500

    Update pafricana_purgehap.sh

--- pafricana_purgehap.sh
+++ pafricana_purgehap.sh
@@ -18,9 +18,9 @@ module load MUMmer/4.0.2
 module load perl/5.28.1
 module load R/3.6.0
 
-# /isg/shared/apps/purge_haplotigs/1.0/bin/purge_haplotigs readhist -b /labs/Wegrzyn/Moss/Physcomitrellopsis_africana/Physcomitrellopsis_africana_Genome/RawData_Nanopore_5074/5074_test_LSK109_30JAN19/physcomitrellopsis_africana_polished_3kb_rmvcomtam_genome.reads.sorted.bam -g /labs/Wegrzyn/Moss/Physcomitrellopsis_africana/Physcomitrellopsis_africana_Genome/RawData_Nanopore_5074/5074_test_LSK109_30JAN19/physcomitrellopsis_africana_polished_3kb_rmvcontam_assembly.fasta -t 25
+# /isg/shared/apps/purge_haplotigs/1.0/bin/purge_haplotigs readhist -b /projects/EBP/Wegrzyn/Moss/Physcomitrellopsis_africana/Physcomitrellopsis_africana_Genome/RawData_Nanopore_5074/5074_test_LSK109_30JAN19/physcomitrellopsis_africana_polished_3kb_rmvcomtam_genome.reads.sorted.bam -g /projects/EBP/Wegrzyn/Moss/Physcomitrellopsis_africana/Physcomitrellopsis_africana_Genome/RawData_Nanopore_5074/5074_test_LSK109_30JAN19/physcomitrellopsis_africana_polished_3kb_rmvcontam_assembly.fasta -t 25
 
 # /isg/shared/apps/purge_haplotigs/1.0/bin/purge_haplotigs contigcov -i physcomitrellopsis_africana_polished_3kb_rmvcomtam_genome.reads.sorted.bam.gencov -l 5 -m 60 -h 190
 
-/isg/shared/apps/purge_haplotigs/1.0/bin/purge_haplotigs purge -g physcomitrellopsis_africana_polished_3kb_rmvcontam_assembly.fasta -c coverage_stats.csv -b /labs/Wegrzyn/Moss/Physcomitrellopsis_africana/Physcomitrellopsis_africana_Genome/RawData_Nanopore_5074/5074_test_LSK109_30JAN19/physcomitrellopsis_africana_polished_3kb_rmvcomtam_genome.reads.sorted.bam -t 25
+/isg/shared/apps/purge_haplotigs/1.0/bin/purge_haplotigs purge -g physcomitrellopsis_africana_polished_3kb_rmvcontam_assembly.fasta -c coverage_stats.csv -b /projects/EBP/Wegrzyn/Moss/Physcomitrellopsis_africana/Physcomitrellopsis_africana_Genome/RawData_Nanopore_5074/5074_test_LSK109_30JAN19/physcomitrellopsis_africana_polished_3kb_rmvcomtam_genome.reads.sorted.bam -t 25
 
