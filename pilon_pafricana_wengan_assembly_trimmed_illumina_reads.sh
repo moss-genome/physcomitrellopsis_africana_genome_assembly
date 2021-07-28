32a0fabcdaaeaa05b0dc84260244e74d11730d76
AStarovoitov <50898446+AStarovoitov@users.noreply.github.com>
2021-07-28 16:53:39 -0400

    Create pilon_pafricana_wengan_assembly_trimmed_illumina_reads.sh

---
+++ pilon_pafricana_wengan_assembly_trimmed_illumina_reads.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=pilon_pafricana_wengan_assembly_trimmed_illumina_reads
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --mail-type=ALL
+#SBATCH --mem=185G
+#SBATCH --mail-user=andrew.starovoitov@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+module load pilon/1.24
+
+java -Xmx180G -jar /isg/shared/apps/pilon/1.24/pilon-1.24.jar --genome /projects/EBP/Wegrzyn/Moss/Physcomitrellopsis_africana/Physcomitrellopsis_africana_Genome/RawData_Nanopore_5074/5074_test_LSK109_30JAN19/pafricana_wengan_assembly/wengan_assembly_run2/pafricana_asm_wengan.SPolished.asm.wengan.fasta --bam /projects/EBP/Wegrzyn/Moss/Physcomitrellopsis_africana/Physcomitrellopsis_africana_Genome/RawData_Nanopore_5074/5074_test_LSK109_30JAN19/pilon/sorted_pafricana_wengan_assembly_trimmed_illumina_reads_trimmed_DP8400020490TR_L01_28.sam.bam --bam /projects/EBP/Wegrzyn/Moss/Physcomitrellopsis_africana/Physcomitrellopsis_africana_Genome/RawData_Nanopore_5074/5074_test_LSK109_30JAN19/pilon/sorted_pafricana_wengan_assembly_trimmed_illumina_reads_trimmed_DP8400020490TR_L01_117.sam.bam --output pafricana_wengan_assembly_trimmed_illumina_reads --outdir pilon_pafricana_wengan_assembly_trimmed_illumina_reads --changes --tracks
+
